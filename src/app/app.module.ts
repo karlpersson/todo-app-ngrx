9576c11f4e5fbeb57fe795012c95c9c0b2c307ab
Karl Persson <karl.persson@essvision.se>
2018-08-15 16:01:35 +0200

    devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {StoreModule} from '@ngrx/store';
 import { todos } from './redux/reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, StoreModule.forRoot({ todos })],
+  imports: [BrowserModule, FormsModule, StoreModule.forRoot({ todos }),  StoreDevtoolsModule.instrument({
+    maxAge: 25 //  Retains last 25 states
+  })],
   providers: [],
   bootstrap: [AppComponent]
 })
